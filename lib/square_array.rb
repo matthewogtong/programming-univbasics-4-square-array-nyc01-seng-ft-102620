f22dbe4f173d8a204adf7cdee34b80a2315d5ce1
Matthew Ogtong <ogtongm@gmail.com>
2020-10-02 18:55:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,21 @@
+# def square_array(array)
+#
+#   counter = 0
+#
+#   while counter < array.length do
+#     array[counter] = array[counter]**2
+#     counter += 1
+#   end
+#   array
+# end
+
+
 def square_array(array)
 
-  counter = 0
-
-  while counter < array.length do
-    array[counter] = array[counter]**2
-    counter += 1
+  array.length.times do |index|
+    array[index] = array[index]**2
   end
   array
+
+
 end
